96aa8b19114cb1083b61a42da2ca5cac47e647d6
Mateusz Fibich <mateusz.fibich@gmail.com>
2021-05-13 21:00:55 +0200

    function refactored

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-let amountElement = document.querySelector(".js-form__amount")
-let currencyElement = document.querySelector(".js-form__currency")
-let formElement = document.querySelector(".js-form")
-let cashElement = document.querySelector(".js-form__cash")
+{
+    const calculateResult = () => {
+        const amountElement = document.querySelector(".js-form__amount")
+        const cashElement = document.querySelector(".js-form__cash")
+        const currencyElement = document.querySelector(".js-form__currency")
+        const amount = amountElement.value;
+        const currency = currencyElement.value;
+        const cash = amount / currency
+        cashElement.innerText = cash.toFixed(2)
+    }
 
+    const formElement = document.querySelector(".js-form")
 
-formElement.addEventListener("submit", (event) => {
-event.preventDefault();
-
-let amount = amountElement.value;
-let currency = currencyElement.value;
-
-let cash = amount / currency
-
-cashElement.innerText = cash.toFixed(2)
-
-})
+const init = () => {
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        calculateResult();
+    })
+}
+init();
+}
